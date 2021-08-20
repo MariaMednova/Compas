09d04e123fc7bb80cd696ffd037e63807c869f88
MariaMednova <maeria.guseynova@gmail.com>
2021-08-20 21:52:54 +0300

    Реализация домашнего задания 8 рейсы finish sort from long to short stringlength

--- src/flight/Flight.java
+++ src/flight/Flight.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class Flight {
     public static void main(String[] args) {
         List<Luggage> list = new ArrayList<>();
@@ -16,13 +17,22 @@ public class Flight {
         list.add(new Luggage("Aeroflot", "Business", 60));
         list.add(new Luggage("S7", "Business", 50));
 
-            Comparator<Luggage> pc = Comparator.comparingInt(p -> p.weight);
-       pc.compare(list.get(0),list.get(1));
-
+        Comparator<Luggage> pc = Comparator.comparingInt(p -> p.weight);
+        pc.compare(list.get(0), list.get(1));
         list.sort(pc);
 
+        Comparator<Luggage> strLength = new Comparator<Luggage>() {
+        @Override
+           public int compare(Luggage o1, Luggage o2) {
+               return ((o2.companyName.concat(o2.flightClass)).length() - ((o1.companyName.concat(o1.flightClass)).length()));
+           }
+       };
+           strLength.compare(list.get(0),list.get(1));
+
+        list.sort(strLength);
 
         System.out.println(list);
+
         }
     }
 
